74048437a942b61329143ba2d54fae96b7c0392b
volokhinn <csgomx@mail.ru>
2023-06-29 02:03:45 +0700

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Events from './pages/Events'
+import Balance from './pages/Balance';
+import BlackList from './pages/BlackList';
+import Notes from './pages/Notes';
+import Staff from './pages/Staff';
 import styles from './scss/index.module.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.container}>
-      <Header />
-      <Home />
-    </div>
+    <Header />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="balance" element={<Balance />} />
+    <Route path="events" element={<Events />} />
+    <Route path="blacklist" element={<BlackList />} />
+    <Route path="notes" element={<Notes />} />
+    <Route path="staff" element={<Staff />} />
+    </Routes>
+    </BrowserRouter>
+</div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,9 @@ import eventsIcon from '../icons/eventsicon.svg';
 import blacklistIcon from '../icons/blacklisticon.svg';
 import exitIcon from '../icons/exiticon.svg';
 import styles from '../scss/components/_home.module.scss';
+import { Link } from 'react-router-dom';
 
-export const Home = () => {
+const Home = () => {
   const pageCardValues = [
     {
       icon: staffIcon,
@@ -36,8 +37,15 @@ export const Home = () => {
     },
   ];
 
-  const pageCards = pageCardValues.map((obj) => <Pagecard key={new Date().getTime()} {...obj} />);
-  return <div className={styles.content}>{pageCards}</div>;
+  const pageCards = pageCardValues.map((obj) => (
+    <Link style={{ textDecoration: 'none' }} key={new Date().getTime()} to={obj.title}>
+      <Pagecard key={new Date().getTime()} {...obj} />
+    </Link>
+  ));
+  return (
+  <div className={styles.content}>{pageCards}</div>
+  
+  );
 };
 
 export default Home;
